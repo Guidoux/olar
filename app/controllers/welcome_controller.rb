b8e439fdbb1c73a4fcc8866d1fe35bfbd2fa9b1b
guidoux <pedroguidoux@gmail.com>
2016-10-09 09:49:35 -0300

    Redirect to the new thanks action after sign in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   layout 'landing_page'
+  before_action :authenticate_user!, :only => :thanks
   
   def home
+  end
+
+  def thanks
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, only: :omniauth_callbacks, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   
   scope "(:locale)", locale: /#{I18n.available_locales.join('|')}/ do 
+    get 'welcome/thanks'
     devise_for :users, skip: :omniauth_callbacks
     root 'welcome#home'
   end
