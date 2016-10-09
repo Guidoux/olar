178de9cbba038816536a2a47c6baf124760ba060
guidoux <pedroguidoux@gmail.com>
2016-10-09 09:05:22 -0300

    First deploy configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
       user.profile_picture = auth.info.image
-      user.location = auth.extra.raw_info.location.name
+      #user.location = auth.extra.raw_info.location.name
       user.locale = auth.extra.raw_info.locale
       user.first_name = auth.extra.raw_info.first_name
       user.last_name = auth.extra.raw_info.last_name
-      user.hometown = auth.extra.raw_info.hometown.name
+      #user.hometown = auth.extra.raw_info.hometown.name
     end
   end
 
